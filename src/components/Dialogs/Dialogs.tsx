d3c73fc83d6e328c6f6eba87009d09c62ba21b80
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-01-19 14:48:26 +0100

    Created users box

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,18 @@
 import globStl from './../global.module.css';
+import dStl from './dialogs.module.css';
 
 export const Dialogs = () => {
   return (
-    <div className={'dialogs'}>
+    <div className={dStl.dialogs}>
       <div className={globStl.container}>
-        <h1>Dialogs</h1>
+        <div className={dStl.dialogsWrap}>
+          <aside className={dStl.dialogsList}>
+<h2 className={dStl.dialogsListHeading}>Messages</h2>
+          </aside>
+          <div className={dStl.dialogsMsgs}>
+
+          </div>
+        </div>
       </div>
 
     </div>
