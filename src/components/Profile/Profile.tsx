bf27107922cf93f7b4fd22765cc236ec650cba83
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-01-20 13:28:14 +0100

    profile info created

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,8 @@
 import globStl from './../global.module.css';
 import sProf from './profile.module.css';
 import avatar from './ava.jpg';
+import Icofont from "react-icofont";
+import React from "react";
 
 export const Profile = () => {
   return (
@@ -10,6 +12,45 @@ export const Profile = () => {
 <div className={sProf.img}>
   <img src={avatar} alt="Avatar"/>
 </div>
+          <div className={sProf.stat}>
+<h2 className={sProf.name}>Name Surname</h2>
+            <p className={sProf.subtitle}>Lviv, Ukraine</p>
+            <div className={sProf.socialMeta}>
+
+              <div className={sProf.social}>
+                <a className={sProf.link} href="#" target="_blank" style={{backgroundColor: "#365493"}}>
+                  <Icofont icon="facebook"/>
+                </a>
+                <a className={sProf.link} href="#" target="_blank" style={{backgroundColor: "#38b8ff"}}>
+                  <Icofont icon="twitter"/>
+                </a>
+                <a className={sProf.link} href="#" target="_blank" style={{backgroundColor: "#ec407a"}}>
+                  <Icofont icon="dribble"/>
+                </a>
+                <a className={sProf.link} href="#" target="_blank" style={{backgroundColor: "#2962ff"}}>
+                  <Icofont icon="behance"/>
+                </a>
+                <a className={sProf.link} href="#" target="_blank" style={{backgroundColor: "red"}}>
+                  <Icofont icon="brand-youtube"/>
+                </a>
+              </div>
+              <div className={sProf.meta}>
+<div className={sProf.metaInfo}>
+  Posts
+  <span>4</span>
+</div>
+                <div className={sProf.metaInfo}>
+                  Comments:
+                  <span> 12</span>
+                </div>
+                <div className={sProf.metaInfo}>
+                  Views:
+                  <span> 12 302</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
