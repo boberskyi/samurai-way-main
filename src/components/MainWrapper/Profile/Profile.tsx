406accbc799d46f8717bcf5d2c17e9d908f6d128
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-02 20:01:08 +0100

    profile components & styled components

--- src/components/MainWrapper/Profile/Profile.tsx
+++ src/components/MainWrapper/Profile/Profile.tsx
@@ -3,58 +3,186 @@ import avatar from './ava.jpg';
 import Icofont from "react-icofont";
 import React from "react";
 import {Likes} from "../../Header/Likes/Likes";
+import styled from "styled-components";
+import {Container} from "../MainWrapper";
+import profileCover from "./profile-cover.jpg";
+import roundShape from "./round_shape1.png";
 
 export const Profile = () => {
   return (
-    <div className={sProf.main}>
-      <div className={"container"}>
-        <div className={sProf.header}>
-          <div className={sProf.img}>
-            <img src={avatar} alt="Avatar"/>
-          </div>
-          <div className={sProf.stat}>
-            <h2 className={sProf.name}>Name Surname</h2>
-            <p className={sProf.subtitle}>Lviv, Ukraine</p>
-            <div className={sProf.socialMeta}>
-
-              <div className={sProf.social}>
-                <a className={sProf.link} href="src/components/MainWrapper/Profile/Profile#" target="_blank" style={{backgroundColor: "#365493"}}>
-                  <Icofont icon="facebook"/>
-                </a>
-                <a className={sProf.link} href="src/components/MainWrapper/Profile/Profile#" target="_blank" style={{backgroundColor: "#38b8ff"}}>
-                  <Icofont icon="twitter"/>
-                </a>
-                <a className={sProf.link} href="src/components/MainWrapper/Profile/Profile#" target="_blank" style={{backgroundColor: "#ec407a"}}>
-                  <Icofont icon="dribble"/>
-                </a>
-                <a className={sProf.link} href="src/components/MainWrapper/Profile/Profile#" target="_blank" style={{backgroundColor: "#2962ff"}}>
-                  <Icofont icon="behance"/>
-                </a>
-                <a className={sProf.link} href="src/components/MainWrapper/Profile/Profile#" target="_blank" style={{backgroundColor: "red"}}>
-                  <Icofont icon="brand-youtube"/>
-                </a>
-              </div>
-              <div className={sProf.meta}>
-                <div className={sProf.metaInfo}>
-                  Posts
-                  <span> 4</span>
-                </div>
-                <div className={sProf.metaInfo}>
-                  Comments:
-                  <span> 12</span>
-                </div>
-                <div className={sProf.metaInfo}>
-                  Views:
-                  <span> 12 302</span>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-        </div>
-        <Likes />
-      </div>
-    </div>
+      <ProfileS>
+        <Container>
+          <ProfileHeaderS>
+            <ProfileImgS>
+              <img src={avatar} alt="Avatar"/>
+            </ProfileImgS>
+            <ProfileStatS>
+              <ProfileNameS>Bohdan Boberskyi</ProfileNameS>
+              <ProfileSubtitleS>Rzeszów, Poland</ProfileSubtitleS>
+              <ProfileSocialMetaS>
+                <ProfileSocialS>
+                  <ProfileSocialLinkS
+                    href="#"
+                    target="_blank"
+                    style={{backgroundColor: "#365493"}}
+                  >
+                    <Icofont icon="facebook"/>
+                  </ProfileSocialLinkS>
+                  <ProfileSocialLinkS
+                    href="#"
+                    target="_blank"
+                    style={{backgroundColor: "#38b8ff"}}
+                  >
+                    <Icofont icon="twitter"/>
+                  </ProfileSocialLinkS>
+                  <ProfileSocialLinkS
+                    href="#"
+                    target="_blank"
+                    style={{backgroundColor: "#ec407a"}}
+                  >
+                    <Icofont icon="dribble"/>
+                  </ProfileSocialLinkS>
+                  <ProfileSocialLinkS
+                    href="#"
+                    target="_blank"
+                    style={{backgroundColor: "#2962ff"}}
+                  >
+                    <Icofont icon="behance"/>
+                  </ProfileSocialLinkS>
+                  <ProfileSocialLinkS
+                    href="#"
+                    target="_blank"
+                    style={{backgroundColor: "red"}}
+                  >
+                    <Icofont icon="brand-youtube"/>
+                  </ProfileSocialLinkS>
+                </ProfileSocialS>
+                <ProfileMetaS>
+                  <ProfileMetaInfoS>
+                    Posts
+                    <span> 4</span>
+                  </ProfileMetaInfoS>
+                  <ProfileMetaInfoS>
+                    Comments:
+                    <span> 12</span>
+                  </ProfileMetaInfoS>
+                  <ProfileMetaInfoS>
+                    Views:
+                    <span> 30 129</span>
+                  </ProfileMetaInfoS>
+                </ProfileMetaS>
+              </ProfileSocialMetaS>
+            </ProfileStatS>
+          </ProfileHeaderS>
+        </Container>
+      </ProfileS>
   )
 }
+
+const ProfileS = styled.div`
+
+`
+const ProfileHeaderS = styled.div`
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 8px;
+  padding: 180px 35px 35px 50px;
+  position: relative;
+  margin-bottom: 30px;
+  z-index: 0;
+  background-image: url(${profileCover});
+  display: flex;
+  align-items: flex-end;
+
+  &:before {
+    content: "";
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(to top, rgba(0, 0, 0, .9), transparent);
+    border-radius: 8px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`
+const ProfileImgS = styled.div`
+  position: relative;
+  margin-right: 30px;
+  border-radius: 50%;
+  padding: 5px;
+  display: inline-block;
+  width: 125px;
+
+  &:after {
+    content: url(${roundShape});
+    position: absolute;
+    top: -4px;
+    left: -4px;
+  }
+
+  img {
+    border-radius: 50%;
+    width: 125px;
+    height: 125px;
+  }
+`
+const ProfileStatS = styled.div`
+  position: relative;
+  width: 100%;
+`
+const ProfileNameS = styled.h2`
+  display: flex;
+  font-size: 26px;
+  font-weight: 700;
+  line-height: 24px;
+  margin-bottom: 2px;
+  position: relative;
+  color: #fff;
+  text-transform: capitalize;
+`
+const ProfileSubtitleS = styled.p`
+  font-size: 14px;
+  max-width: 550px;
+  line-height: 28px;
+  color: #fff;
+  margin-bottom: 10px;
+`
+const ProfileSocialMetaS  = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const ProfileSocialS = styled.div`
+  display: flex;
+  gap: 5px;
+`
+const ProfileSocialLinkS = styled.a`
+  text-align: center;
+  font-size: 18px;
+  color: #fff;
+  height: 35px;
+  width: 35px;
+  line-height: 35px;
+  border-radius: 50%;
+  transition: all .3s ease-in-out;
+  &:hover {
+    opacity: .8;
+    transition: all .3s ease-in-out;
+  }
+`
+const ProfileMetaS = styled.div`
+  display: flex;
+  gap: 25px;
+`
+const ProfileMetaInfoS = styled.div`
+  color: #d7d7d7;
+  font-size: 14px;
+  span {
+    font-weight: 500;
+    color: #fff;
+  }
+`
+
+
+
