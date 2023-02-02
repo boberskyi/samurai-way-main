b497fcea39affd861681dc0fc0392324ad3fbb6d
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-02 16:18:04 +0100

    Music components and styled components

--- src/components/MainWrapper/Music/Music.tsx
+++ src/components/MainWrapper/Music/Music.tsx
@@ -1,10 +1,14 @@
+import styled from "styled-components";
+import {Container} from "../MainWrapper";
 
 export const Music = () => {
   return (
-    <div className={'music'}>
-      <div className={"container"}>
-        <h1>Music</h1>
-      </div>
-    </div>
+    <MusicS>
+      <Container><h1>Music</h1></Container>
+    </MusicS>
   )
 }
+
+const MusicS = styled.div`
+  
+`
