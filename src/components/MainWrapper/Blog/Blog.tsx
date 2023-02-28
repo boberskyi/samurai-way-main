1129ab1ec42cb331b5d1e55818c10753bb0d1c2a
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-28 10:55:12 +0100

    Added input and btn for blog

--- src/components/MainWrapper/Blog/Blog.tsx
+++ src/components/MainWrapper/Blog/Blog.tsx
@@ -2,13 +2,23 @@ import {Post} from "./Post/Post";
 import {StyledContainer} from "../MainWrapper";
 import styled from "styled-components";
 import {BlogPropsType} from "../../../types";
+import React from "react";
 
 
+const postTitle = React.createRef<HTMLInputElement>();
 
 export const Blog = (props:BlogPropsType) => {
+
+  const addPosts = () => {
+  const text = postTitle.current?.value;
+    alert(text);
+  }
   return (
     <StyledBlog>
       <StyledContainer>
+        <button onClick={addPosts}>Add post</button>
+        <input type="text" placeholder={"Title"} ref={postTitle}/>
+
         <Post posts={props.posts}/>
       </StyledContainer>
     </StyledBlog>
