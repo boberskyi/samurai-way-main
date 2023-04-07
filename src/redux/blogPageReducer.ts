3f99dcffe68387b55cf0ef4e7295401b1629ac9d
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-04-07 13:30:21 +0200

    blogPageReducer

--- src/redux/blogPageReducer.ts
+++ src/redux/blogPageReducer.ts
@@ -2,18 +2,23 @@ import {PostsPropsType} from "../types";
 import {BlogPagePropsType} from "./state";
 
 export const blogPageReducer = (state:BlogPagePropsType, action:any) => {
-  if (action.type === 'ADD-POST') {
-    const newPost: PostsPropsType = {
-      id: new Date().getTime(),
-      img: 'https://via.placeholder.com/350x150',
-      title: state.messageForNewPost,
-      descript: 'Lorem ipsum lalalala'
+  switch(action.type) {
+    case 'ADD-POST': {
+      const newPost: PostsPropsType = {
+        id: new Date().getTime(),
+        img: 'https://via.placeholder.com/350x150',
+        title: state.messageForNewPost,
+        descript: 'Lorem ipsum lalalala'
+      }
+      state.posts.push(newPost);
+      state.messageForNewPost = '';
+      return state;
     }
-    state.posts.push(newPost);
-    state.messageForNewPost = '';
-  }  else if (action.type === 'CHANGE-TEXT') {
-    state.messageForNewPost = action.newText;
+    case 'CHANGE-TEXT': {
+      state.messageForNewPost = action.newText;
+      return state;
+    }
+    default: return state;
   }
 
-  return state;
 }
